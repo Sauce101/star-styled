eb388e274a05fc34feed8b32a6a03f209fadafd5
Sauce101 <michaelsaucedo443@gmail.com>
2022-01-18 02:11:37 -0800

    changed footer text again

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,11 +9,6 @@ import {
 } from "react-icons/fa";
 import {
   FooterContainer,
-  // FooterLink,
-  // FooterLinkItems,
-  // FooterLinksContainer,
-  // FooterLinksWrapper,
-  // FooterLinkTitle,
   FooterWrap,
   SocialIconLink,
   SocialIcons,
@@ -31,50 +26,12 @@ const Footer = () => {
   return (
     <FooterContainer>
       <FooterWrap>
-        {/* <FooterLinksContainer>
-          <FooterLinksWrapper>
-            <FooterLinkItems>
-              <FooterLinkTitle>About us</FooterLinkTitle>
-              <FooterLink to="/">How it works</FooterLink>
-              <FooterLink to="/">Testimonials</FooterLink>
-              <FooterLink to="/">Carrers</FooterLink>
-              <FooterLink to="/">Investor</FooterLink>
-              <FooterLink to="/">Terms of Service</FooterLink>
-            </FooterLinkItems>
-            <FooterLinkItems>
-              <FooterLinkTitle>Contact Us</FooterLinkTitle>
-              <FooterLink to="/">Contact</FooterLink>
-              <FooterLink to="/">Support</FooterLink>
-              <FooterLink to="/">Destinations</FooterLink>
-              <FooterLink to="/">Sponsorships</FooterLink>
-            </FooterLinkItems>
-          </FooterLinksWrapper>
-
-          <FooterLinksWrapper>
-            <FooterLinkItems>
-              <FooterLinkTitle>Videos</FooterLinkTitle>
-              <FooterLink to="/">Submit Video</FooterLink>
-              <FooterLink to="/">Ambassadors</FooterLink>
-              <FooterLink to="/">Agency</FooterLink>
-              <FooterLink to="/">Influencer</FooterLink>
-            </FooterLinkItems>
-            <FooterLinkItems>
-              <FooterLinkTitle>Social Media</FooterLinkTitle>
-              <FooterLink to="/">Instagram</FooterLink>
-              <FooterLink to="/">Facebook</FooterLink>
-              <FooterLink to="/">Youtube</FooterLink>
-              <FooterLink to="/">Twitter</FooterLink>
-            </FooterLinkItems>
-          </FooterLinksWrapper>
-        </FooterLinksContainer> */}
         <SocialMedia>
           <SocialMediaWrap>
             <SocialLogo to="/" onClick={toggleHome}>
               DS-1
             </SocialLogo>
-            <WebsiteRights>
-              DS-1 © 2021 - {new Date().getFullYear()}
-            </WebsiteRights>
+            <WebsiteRights>© 2021 Michael Saucedo</WebsiteRights>
             <SocialIcons>
               <SocialIconLink href="/" target="_blank" aria-label="Facebook">
                 <FaFacebook />
